bf3a1b0dfba290403e1524d2bd7d03b93595ccc6
Fernando Castelan <fercc1097@gmail.com>
2022-01-07 13:27:32 -0600

    added menu

--- components/Header/index.jsx
+++ components/Header/index.jsx
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './index.module.scss';
 import Image from 'next/image';
 
-const index = () => {
+const Options = ({ onClose }) => {
     return (
-        <div className={styles.menuContainer}>
-            <p className={styles.logo}>Aquara</p>
-            <Image src={'/icons/burger-menu.svg'} width={50} height={50} />
+        <div className={styles.optionsContainer}>
+            <a className={styles.option} href="#rent">Renta</a>
+            <a className={styles.option} href="#sale">Venta</a>
+            <a className={styles.option} href="#contact">Contacto</a>
+            <p onClick={onClose} className={styles.option}>Cerrar</p>
+        </div>
+    );
+};
+
+const index = () => {
+    const [isOpen, setIsOpen] = useState(false);
+    const onClick = () => {
+        setIsOpen(!isOpen);
+    };
+    return (
+        <div className={styles.menu}>
+            <div className={styles.menuContainer}>
+                <p className={styles.logo}>Aquara</p>
+                <Image onClick={onClick} src={'/icons/burger-menu.svg'} width={50} height={50} />
+            </div>
+            {isOpen && <Options onClose={onClick} />}
         </div>
     );
 };
